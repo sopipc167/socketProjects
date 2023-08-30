9cac1b2b21872809ec05baa664f9301ae8bad497
sopipc167 <sopipc167@gmail.com>
2023-08-31 01:05:01 +0900

    소켓 그림판 완성

--- socketPaintboard/app.js
+++ socketPaintboard/app.js
@@ -5,41 +5,51 @@ const ejs = require('ejs');
 const fs = require('fs');
 
 const app = express();
-app.use(app.router);
+//app.use(app.router);
 app.use(express.static('public'));
 
 var server = http.createServer(app);
+
+var roomlist = [];
 server.listen(8005,()=>{
     console.log('server running...');
 })
 
-var io = socketio.listen(server);
-io.set('log level',2);
+var io = socketio(server);
+//io.set('log level',2);
 
 app.get('/',(req,res)=>{
     fs.readFile('lobby.html', function(error,data){
-        express.response.send(data.toString());
+        res.send(data.toString());
     });
 });
 
 app.get('/canvas/:room', (req,res)=>{
     fs.readFile('canvas.html','utf-8', (err,data)=>{
-        express.response.send(ejs.render(data,{
+        res.send(ejs.render(data,{
             room: req.param('room')
         }));
     });
 });
 
 app.get('/room',(req,res)=>{
-    express.response.send(io.sockets.manager.rooms);
+    res.send(JSON.stringify(roomlist));
+    console.log(JSON.stringify(roomlist));
 });
 
-io.sockets.on('connection',(socket)=>{
+io.on('connection',(socket)=>{
+    
     socket.on('join',(data) => {
         socket.join(data);
-        socket.set('room',data);
+        socket.room = data;
     });
+    socket.on('draw', function (data) {
+        io.sockets.in(socket.room).emit('line', data);
+    });
+    socket.on('create_room',(data)=>{
+        io.sockets.emit('create_room',data.toString());
+        roomlist.push(data.toString());
+        console.log("방목록은:"+roomlist);
+    })
 });
-socket.on('create_room',(data)=>{
-    io.sockets.emit('create_room',data.toString());
-})
+
